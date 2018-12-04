dc9fae53bebd4a8137b2f761734c98812daa50d8
Afam <afamezechukwu@gmail.com>
2018-12-04 09:38:56 +0100

    Added Listings and Realtors

---
+++ realtors/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'email', 'hire_date', 'is_mvp')
+  list_display_links = ('id', 'name')
+  list_filter = ('hire_date',)
+  list_editable = ('is_mvp',)
+  search_fields = ('id', 'name', 'email', 'hire_date')
+  list_per_page = 25
+
+admin.site.register(Realtor, RealtorAdmin)
